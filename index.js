4b80cc87c60abae8d9967d5556c343fd5e214444
pasukanbocil <ctian8787@gmail.com>
2024-05-12 17:01:57 +0700

    chore: Add MongoDB connection in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/movie_db")
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
