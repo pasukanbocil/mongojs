e7fe3dce35d8b99431e641b18130aded1649bfe6
pasukanbocil <ctian8787@gmail.com>
2024-05-13 10:26:12 +0700

    Add movie models and insert multiple movies in index.js

--- index.js
+++ index.js
@@ -11,19 +11,57 @@ mongoose
 const movieSchema = new mongoose.Schema({
   title: String,
   year: Number,
-  score: Number,
+  genre: String,
   director: String,
+  rating: Number,
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
 
-const movie = new Movie({
-  title: "Black Panther",
-  year: 2018,
-  score: 7.3,
-  director: "Dicky",
-});
+// const movie = new Movie({
+//   title: "Black Panther",
+//   year: 2018,
+//   score: 7.3,
+//   director: "Dicky",
+// });
 
-movie.save();
+Movie.insertMany([
+  {
+    title: "Avengers: Infinity War",
+    genre: "Action",
+    director: "Anthony Russo, Joe Russo",
+    year: 2018,
+    rating: 8.4,
+  },
+  {
+    title: "Joker",
+    genre: "Crime",
+    director: "Todd Phillips",
+    year: 2019,
+    rating: 8.4,
+  },
+  {
+    title: "Parasite",
+    genre: "Drama",
+    director: "Bong Joon Ho",
+    year: 2019,
+    rating: 8.6,
+  },
+  {
+    title: "Spider-Man: Into the Spider-Verse",
+    genre: "Animation",
+    year: 2018,
+    rating: 8.4,
+  },
+])
+  .then((result) => {
+    console.log("it works");
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-console.log(movie);
+// movie.save();
+
+// console.log(movie);
